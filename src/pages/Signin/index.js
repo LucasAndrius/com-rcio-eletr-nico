638f7b02e516097b3ba68664ad229a31b7e27167
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-02-24 22:11:54 -0300

    adicionando funcionalidades no login

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,36 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {PageArea} from './styled';
 
-import {PageContainer, PageTitle, } from '../../components/MainComponents';
+import useApi from '../../helpers/OlxAPI';
+import { doLogin } from '../../helpers/AuthHandler';
+
+import {PageContainer, PageTitle, ErrorMessage } from '../../components/MainComponents';
+
 
 const Page = () =>{
+
+    const api = useApi();
+
+    const [ email, setEmail] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ rememberPassword, setRememberPassword] = useState(false);
+    const [disabled, setDesabled] = useState(false);
+    const [ error, setError] = useState('');
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setDesabled(true);
+
+        const json = await api.login(email, password);
+
+        if(json.error){
+            setError(json.error);
+        } else {
+            doLogin(json.token, rememberPassword); 
+            window.location.href = '/';
+        }
+    }
+
     return(
         <PageContainer>
             <PageTitle>Login</PageTitle>
             <PageArea>
-                <form>
+                {error &&
+                    <ErrorMessage/>
+                }
+
+                <form onSubmit={handleSubmit}>
                     <label className="area">
                         <div className="area--title">E-mail</div>
                         <div className="area--input">
-                            <input type="email"/>
+                            <input 
+                                type="email" 
+                                disabled={disabled}
+                                value={email}
+                                onChange={e=>setEmail(e.target.value)}
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Senha</div>
                         <div className="area--input">
-                            <input type="password"/>
+                            <input 
+                                type="password" 
+                                disabled={disabled}
+                                value={password}
+                                onChange={e=>setPassword(e.target.value)}
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title">Lembrar Senha</div>
                         <div>
-                            <input type="checkbox"/>
+                            <input 
+                                type="checkbox" 
+                                disabled={disabled}
+                                defaultChecked={rememberPassword}
+                                onClick={ ()=>setRememberPassword(!rememberPassword)}
+                            />
                         </div>
                     </label>
                     <label className="area">
                         <div className="area--title"></div>
                         <div className="area--input">
-                            <button>Fazer Login</button>
+                            <button 
+                                disabled={disabled}>
+                                    Fazer Login
+                            </button>
                         </div>
                     </label>
                 </form>
