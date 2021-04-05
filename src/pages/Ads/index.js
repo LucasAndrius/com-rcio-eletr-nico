d2b146345d7321aa09881efe26c0cd6cd302f488
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-05 15:00:37 -0300

    criando filtro funcionalidades

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -4,13 +4,23 @@ import {PageArea} from './styled';
 import useApi from '../../helpers/OlxAPI';
 
 import {PageContainer } from '../../components/MainComponents';
-import { Link } from 'react-router-dom';
+import { useLocation,useHistory } from 'react-router-dom';
 import AdItem from '../../components/partials/AdItem';
 
 
 const Page = () =>{
 
     const api = useApi();
+
+    const useQueryString = () =>{
+        return new URLSearchParams( useLocation().search);
+    }
+
+    const query =   useQueryString();
+
+    const [q, setQ] = useState( query.get('q') !=null ? query.get('q') : '' );
+    const [cat, setCat] = useState(query.get('cat') !=null ? query.get('cat') : '' );
+    const [state, setState] = useState(query.get('state') !=null ? query.get('state') : '' );
 
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -50,9 +60,14 @@ const Page = () =>{
                 <PageArea>
                     <div className="leftSide">
                        <form method="GET">
-                            <input  type="text" name="q" />
+                            <input  
+                                type="text" 
+                                name="q" 
+                                placeholder="O que você procura?" 
+                                value={q}
+                            />
                             <div className="filterName">Estado:</div>
-                            <select name="state">
+                            <select name="state" value={state}>
                                 <option></option>
                                 {stateList.map((i,k)=>
                                     <option key={k} value={i.name}>{i.name}</option>
@@ -62,7 +77,7 @@ const Page = () =>{
                             <div className="filterName">Categoria:</div>
                             <ul>
                                 { categories.map((i,k)=>
-                                    <li key={k} className="categoryItem">
+                                    <li key={k} className={cat == i.slug?'categoryItem active' : 'categoryItem'}>
                                         <img src={i.img} alt/>
                                         <span>{i.name}</span>
                                     </li>
--- src/pages/Ads/styled.js
+++ src/pages/Ads/styled.js
@@ -2,16 +2,64 @@ import styled from 'styled-components';
 
 export const PageArea = styled.div`
 display:flex;
-margint-top:20px;
+margin-top:20px;
     .leftSide{
         width:250px;
         margin-right:10px;
+
+        .filterName{
+            font-size:1rem;
+            margin: 10px 0;
+        }
+
+        input, select{
+            width:100%;
+            height:40px;
+            border:2px solid #9bb83c;
+            border-radius:5px;
+            outline:0;
+            font-size:15px;
+            color:#111;
+            padding:10px;
+
+        }
+
+        ul, li{
+            margin:0;
+            padding:0;
+            list-style:none;
+        }
+
+        .categoryItem{
+            display:flex;
+            align-items:center;
+            padding:10px;
+            border-radius:5px;
+            color:#111;
+            cursor:pointer;
+
+           
+
+            img{
+                width:25px;
+                height:25px;
+                margin-right:5px;
+            }
+            span{
+                font-size:14px;
+
+            }
+        }
+
+        .categoryItem:hover,
+        .categoryItem.active{
+            background-color:#9bb83c;
+            color:#fff;
+        }
     }
     .rightSide{
         flex:1;
     }
 
-    .categoryItem{
-        
-    }
+    
 `;
